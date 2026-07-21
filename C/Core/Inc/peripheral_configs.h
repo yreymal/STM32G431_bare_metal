a5fcd1990b842f6906dfd18063db972555d0e291
Roman Yarmak <61269384+yreymal@users.noreply.github.com>
2026-07-21 17:53:41 +0200

    Revert "Porting c code into cpp"

--- C/Core/Inc/peripheral_configs.h
+++ C/Core/Inc/peripheral_configs.h
@@ -12,25 +12,6 @@
 #include <stm32g431xx.h>
 #include "status.h"
 #include "reg_bits_check.h"
-#include "prescalers.h"
-
-#define HSE_HZ        24000000UL
-#define PLLM_DIV      3U
-#define PLLR_DIV      2U  /* PLL could be only 2,4,6,8 “R” output clock frequency = VCO frequency / PLLR */
-#define PLLM_DIV_REG_VAL (PLLM_DIV - 1)
-#define PLLR_DIV_REG_VAL ((PLLR_DIV/2U) -1U)
-
-#define PLLN_MUL      25U /* value can be only in rage PLLN 8 =< PLLN =< 127 */
-
-
-
-#define VCO_CLK_HZ  ((HSE_HZ / PLLM_DIV) * PLLN_MUL)  /* 200000000UL */
-#define SYSCLK_HZ   (VCO_CLK_HZ / PLLR_DIV)              /* 100000000UL */
-
-#define AHB_CLOCK_VALUE_HZ            (SYSCLK_HZ / AHB_PRESCALER_VALUE)
-#define APB1_CLOCK_VALUE_HZ           (AHB_CLOCK_VALUE_HZ / APB1_PRESCALER_VALUE)
-#define APB2_CLOCK_VALUE_HZ           (AHB_CLOCK_VALUE_HZ / APB2_PRESCALER_VALUE)
-
 
 /**
   * @brief  The application entry point.
@@ -39,13 +20,11 @@
 
 int configureUserButton();
 
-status_t ConfigureClock(void);
+status_t ConfigureClock();
 
 status_t initGPIOA5();
 
 int delay(const uint32_t timer);
-
-status_t configure_MCO_pinA8();
 
 
 
